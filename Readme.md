d6c697ed0cd87d5afd1631dddb271829f0c49e72
赵禹 <zhaoyu10@tal.com>
2021-10-14 15:59:00 +0800

    add: build

--- Readme.md
+++ Readme.md
@@ -3,5 +3,10 @@
 mkdir temp && cd /temp
 1. 初始化初始化项目；
 2. 生产client。   -- 可以参照Hack中readme.md
-3. 
+3. 拷贝api到pkg/apis中。
+4. devkubectl apply -f  deploy/crd/cluster_crd.yaml 
+5. devkubectl apply -f  deploy/deploy.yaml 
+
+测试
+6. devkubectl apply -f  deploy/crd/cluster_cr.yaml 
 </pre>
--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 export GOPROXY=https://goproxy.cn
-#go mod download
-#go mod vendor
+go mod download
+go mod vendor
 
-cp -r api/ pkg/apis
+GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build -o manager cmd/main.go
+cp -R deploy/Dockerfile Dockerfile.amd64
+docker build -t hub.xesv5.com/zhaoyu10/cluster-mesh:v1.0.2 -f Dockerfile.amd64 .
+docker push hub.xesv5.com/zhaoyu10/cluster-mesh:v1.0.2
